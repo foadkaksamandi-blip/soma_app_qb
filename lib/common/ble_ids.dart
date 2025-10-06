d77606065bba1b896ebbfaddb7c298cf6ba5c7be
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-06 16:16:35 +0330

    Update ble_ids.dart

--- lib/common/ble_ids.dart
+++ lib/common/ble_ids.dart
@@ -1,7 +1,11 @@
+/// UUIDهای ثابت برای سرویس BLE و مشخصه‌های پرداخت
 class BleIds {
-  // UUIDs for the BLE Service and Characteristics
+  /// سرویس اصلی SOMA برای تراکنش‌ها
   static const String serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-  static const String amountCharacteristicUuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-  static const String ackCharacteristicUuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
-  static const String qrCharacteristicUuid = "6e400004-b5a3-f393-e0a9-e50e24dcca9e";
+
+  /// مشخصه ارسال مبلغ از خریدار به فروشنده
+  static const String amountCharUuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+  /// مشخصه دریافت تأیید (ACK) از فروشنده
+  static const String ackCharUuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 }
