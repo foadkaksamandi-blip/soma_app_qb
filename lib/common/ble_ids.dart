6e7d8c4e65986c2c72958e5e67c0059565e1d244
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-06 16:08:37 +0330

    Create ble_ids.dart

---
+++ lib/common/ble_ids.dart
@@ -0,0 +1,7 @@
+class BleIds {
+  // UUIDs for the BLE Service and Characteristics
+  static const String serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+  static const String amountCharacteristicUuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  static const String ackCharacteristicUuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+  static const String qrCharacteristicUuid = "6e400004-b5a3-f393-e0a9-e50e24dcca9e";
+}
