d839e7d3a58ed9f6f799d42b242d8b9b25af72b3
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-06 17:12:57 +0330

    Update ble_ids.dart

--- lib/common/ble_ids.dart
+++ lib/common/ble_ids.dart
@@ -1,6 +1,6 @@
 class BleIds {
-  // UUIDs for SOMA Offline BLE service and its characteristics
+  // Unique UUIDs for SOMA Offline BLE communication
   static const String serviceUuid = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-  static const String amountCharacteristicUuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-  static const String ackCharacteristicUuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+  static const String amountCharUuid = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+  static const String ackCharUuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 }
